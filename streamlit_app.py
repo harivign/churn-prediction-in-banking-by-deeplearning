279a46910281c5b95b97b8930d0a5198021852b3
Hari vignesh K <129162827+harivign@users.noreply.github.com>
2025-02-19 08:01:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
-import tensorflow 
 from tensorflow.keras.models import load_model
 from tensorflow.keras.layers import LeakyReLU
 
